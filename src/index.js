988a4ef38aaaaa5504b55f607678caddfa6df6e4
Jiaqing Wang <jiaqing_wang@comcast.com>
2020-12-01 14:46:45 -0600

    #11: command line parameters should be tested with ===, not includes

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ const checkIfParamsArrayIsEmpty = params => Boolean(params.length)
 
 const checkBufferObject = element => element instanceof Object && element.name && Buffer.isBuffer(element.buffer)
 
-const checkCommandForIO = element => element.includes('-in') || element.includes('-out')
-    || element.includes('-keyout') || element.includes('-signkey') || element.includes('-key')
+const checkCommandForIO = element => element === '-in' || element === '-out'
+    || element === '-keyout' || element === '-signkey' || element === '-key'
 
 const checkDataTypeCompatibility = (params) => {
     const allowedParamsDataTypes = ['string', 'object']
